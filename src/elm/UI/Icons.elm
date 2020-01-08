f07ec58896005df9267fb2edb13eefc217dba600
Igor Brylev <igor-brilev@yandex.ru>
2020-01-08 17:27:05 +0300

    added tool icon

--- src/elm/UI/Icons.elm
+++ src/elm/UI/Icons.elm
@@ -238,6 +238,7 @@ module UI.Icons exposing
     , thumbsUp
     , toggleLeft
     , toggleRight
+    , tool
     , trash
     , trash2
     , trello
@@ -2373,6 +2374,13 @@ thumbsUp : Html msg
 thumbsUp =
     svgFeatherIcon "thumbs-up"
         [ Svg.path [ d "M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" ] []
+        ]
+
+
+tool : Html msg
+tool =
+    svgFeatherIcon "tool"
+        [ Svg.path [ d "M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" ] []
         ]
 
 
